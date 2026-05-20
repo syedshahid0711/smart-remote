78c7be531968a7e942aeba10d987954ab98770e8
Ali <syedshahid0711@gmail.com>
2026-05-20 12:53:44 +0530

    seo: optimized web client for 'nova remote apk' keyword queries

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2339768004"
+    serviceWorkerVersion: "1720426751"
   }
 });
